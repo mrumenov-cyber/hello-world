59a275b4717ec0d0f49d2eaa69b2a595d8b77b83
mrumenov-cyber <marija.rumenovic@fesb.hr>
2022-02-08 15:43:00 +0100

    Exercise 5.2

--- components/Chat.js
+++ components/Chat.js
@@ -1,7 +1,60 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform, KeyboardAvoidingView } from 'react-native';
+import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 
 export default class Chat extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      messages: [],
+    }
+  }
+
+  componentDidMount() {
+    const name = this.props.route.params.name;
+
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello ' + name + ', welcome!', 
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+        {
+          _id: 2,
+          text: name + ' has entered the chat',
+          createdAt: new Date(),
+          system: true,
+         },
+      ],
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }))
+  }
+  
+
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#000'
+          }
+        }}
+      />
+    )
+  }
+
   render() {
     //entered name state from Start screen gets displayed in status bar at the top of the app
     let name = this.props.route.params.name;
@@ -12,19 +65,19 @@ export default class Chat extends React.Component {
     return (
       <View style={{
         flex: 1, 
-        justifyContent: 'flex-start', 
-        alignItems: 'center',
-        backgroundColor: bgColor
+        backgroundColor: bgColor,
         }}>
-        <Text style={{
-          fontSize: 18,
-          color: '#555555',
-          backgroundColor: '#FFFFFF',
-          fontWeight: "400",
-          padding: 20,
-          borderRadius: 40,
-          margin: 15,
-        }}>Hello {name}, Welcome to Chatting!</Text>
+      
+          <GiftedChat
+            messages={this.state.messages}
+            onSend={messages => this.onSend(messages)}
+            renderBubble={this.renderBubble.bind(this)}
+            user={{
+              _id: 1,
+            }}
+          />
+
+          { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
       </View>
     )
   }
