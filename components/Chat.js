06ccb71276e3ebc8fd9ffe3384d5aec36b1bdffe
mrumenov-cyber <marija.rumenovic@fesb.hr>
2022-02-11 11:51:36 +0100

    5.3 exercise added code

--- components/Chat.js
+++ components/Chat.js
@@ -1,44 +1,129 @@
 import React from 'react';
-import { View, Text, Platform, KeyboardAvoidingView } from 'react-native';
+import { View, Text, Platform, KeyboardAvoidingView, FlatList } from 'react-native';
 import { GiftedChat, Bubble } from 'react-native-gifted-chat';
+
+//importing firestore
+const firebase = require('firebase');
+require('firebase/firestore');
+
 
 export default class Chat extends React.Component {
   constructor(){
     super();
     this.state = {
       messages: [],
+      uid: 0,
+      user:{
+        _id: "",
+        name: "",
+        avatar: "",
+      }
     }
-  }
+  
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyDmH3vo0j2EmqzCEX5XEyXm__fujhXZoac",
+      authDomain: "chatapp-dd30d.firebaseapp.com",
+      projectId: "chatapp-dd30d",
+      storageBucket: "chatapp-dd30d.appspot.com",
+      messagingSenderId: "793384653961",
+      appId: "1:793384653961:web:f54e9b3329df412859e258"
+    }
+
+      //initialize app 
+      if (!firebase.apps.length){
+        firebase.initializeApp(firebaseConfig);
+      }
+      //reference firestore database
+      this.referenceChatMessages = firebase.firestore().collection('messages');
+}
+
 
   componentDidMount() {
-    const name = this.props.route.params.name;
+     // Set the page title once Chat is loaded
+     let { name } = this.props.route.params
+     // Adds the name to top of screen
+     this.props.navigation.setOptions({ title: name })
 
-    this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: 'Hello ' + name + ', welcome!', 
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'React Native',
-            avatar: 'https://placeimg.com/140/140/any',
-          },
-        },
-        {
-          _id: 2,
-          text: name + ' has entered the chat',
-          createdAt: new Date(),
-          system: true,
-         },
-      ],
+    this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        firebase.auth().signInAnonymously();
+      }
+      //update user state with currently active user data
+      this.setState({
+        uid: user.uid,
+        loggedInText: 'Hello '+ name,
+        messages: [],
+        user: {
+          _id: user.uid,
+          name: name,
+          avatar: "https://placeimg.com/140/140/any"
+        }
+      });
+      this.unsubscribe = this.referenceChatMessages
+        .orderBy("createdAt", "desc")
+        .onSnapshot(this.onCollectionUpdate);
     });
   }
+
+  onCollectionUpdate = (querySnapshot) => {
+    const messages = [];
+    // go through each document
+    querySnapshot.forEach((doc) => {
+      // get the QueryDocumentSnapshot's data
+      let data = doc.data();
+      messages.push({
+        _id: data._id,
+        text: data.text,
+        createdAt: data.createdAt.toDate(),
+        user: {
+          _id: data.user._id,
+          name: data.user.name,
+          avatar: data.user.avatar
+        }
+      });
+    });
+    this.setState({
+      messages : messages,
+    });
+  };
+
+  //dont receive updates from collection
+  componentWillUnmount() {
+    this.authUnsubscribe();
+    this.unsubscribe();
+  }
+
+
+  //adding new message to database collection
+  addMessage() {
+    const message = this.state.messages[0];
+    
+    this.referenceChatMessages.add({
+      _id: message._id,
+      text: message.text,
+      createdAt: message.createdAt,
+      user: this.state.user
+    });
+  }
+
+
 
   onSend(messages = []) {
     this.setState(previousState => ({
       messages: GiftedChat.append(previousState.messages, messages),
     }))
+  }
+
+  //renderSystemMessage function renders a system message; the text color depends on the set background color
+  renderSystemMessage(props) {
+    const { bgColor } = this.props.route.params;
+    return (
+      <SystemMessage
+        {...props}
+        textStyle= {{color: bgColor === '#B9C6AE' ? '#555555' : '#dddddd'}}
+      />
+    );
   }
   
 
@@ -58,8 +143,8 @@ export default class Chat extends React.Component {
   render() {
     //entered name state from Start screen gets displayed in status bar at the top of the app
     let name = this.props.route.params.name;
-    this.props.navigation.setOptions({ title: name});
-
+  
+    //Color of the screen background becomes the one we picked on the start page
     const { bgColor } = this.props.route.params;
 
     return (
@@ -67,13 +152,16 @@ export default class Chat extends React.Component {
         flex: 1, 
         backgroundColor: bgColor,
         }}>
-      
+          <Text>{this.state.loggedInText}</Text>
           <GiftedChat
             messages={this.state.messages}
             onSend={messages => this.onSend(messages)}
             renderBubble={this.renderBubble.bind(this)}
+            renderSystemMessage={this.renderSystemMessage.bind(this)}
             user={{
-              _id: 1,
+              _id: this.state.user._id,
+              name: this.state.name,
+              avatar: this.state.user.avatar
             }}
           />
 
@@ -82,3 +170,5 @@ export default class Chat extends React.Component {
     )
   }
 }
+
+
